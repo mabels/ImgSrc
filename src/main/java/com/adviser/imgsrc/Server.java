e43e5b335b72fd3a107111defec2150fc2535ae4
Meno Abels <meno.abels@adviser.com>
2011-09-10 17:11:02 +0200

    Remove the HTTPFilterStrategie

--- src/main/java/com/adviser/imgsrc/Server.java
+++ src/main/java/com/adviser/imgsrc/Server.java
@@ -3,13 +3,16 @@ package com.adviser.imgsrc;
 import org.apache.camel.CamelContext;
 import org.apache.camel.component.jetty.JettyHttpComponent;
 import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.impl.DefaultHeaderFilterStrategy;
 
 public class Server {
 
   public static void main(String[] args) {
     final CamelContext camelContext = new DefaultCamelContext();
     camelContext.disableJMX();
-    camelContext.addComponent("jetty", new JettyHttpComponent());
+    JettyHttpComponent jhc = new JettyHttpComponent();
+    jhc.setHeaderFilterStrategy(new DefaultHeaderFilterStrategy());
+    camelContext.addComponent("jetty", jhc);
     try {
       camelContext.addRoutes(new Router(args));
       camelContext.start();
